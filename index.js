13b987a9097045e435f9a47849dceca2f34c86d8
lemanski <lucaslemanski.ofc@gmail.com>
2023-02-19 22:03:50 -0300

    Organização e correção das rotas

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
 
+const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+
+// Rota para retornar todos os veículos
+app.get('/veiculos', async (req, res) => {
+  const vehicles = await prisma.vehicle.findMany();
+  res.json(vehicles);
+});
+
+// Rota para pesquisar por parametro
+app.get('/veiculos/:marca', async (req, res) => {
+  const { marca } = req.params;
+
+  const vehicles = await prisma.vehicle.findMany({
+    where: {
+      marca: marca
+    }
+  });
+  res.json(vehicles);
+});
+
+// Rota para visualizar um veiculo pelo id
+app.get('/veiculos/:id', async (req, res) => {
+  const { id } = req.params;
+  const veiculo = await prisma.veiculo.findUnique({
+    where: { id: parseInt(id) }
+  });
+  res.json(veiculo);
+});
 
 // Rota para criar um novo veículo
 app.post('/veiculos', async (req, res) => {
@@ -19,12 +46,6 @@ app.post('/veiculos', async (req, res) => {
     },
   });
   res.json(newVehicle);
-});
-
-// Rota para retornar todos os veículos
-app.get('/veiculos', async (req, res) => {
-  const vehicles = await prisma.vehicle.findMany();
-  res.json(vehicles);
 });
 
 // Rota para modificar um veículo pelo ID
@@ -46,7 +67,30 @@ app.put('/veiculos/:id', async (req, res) => {
   res.json(updatedVehicle);
 });
 
-//Deleta um veiculo pelo seu ID
+// Rota para alterar algo especifico
+app.patch('/veiculos/:id', async (req, res) => {
+  const { id } = req.params;
+  const { vehicle, marca, ano, descricao, vendido } = req.body;
+  
+  try {
+    const updatedVehicle = await prisma.vehicle.update({
+      where: { id: parseInt(id) },
+      data: {
+        vehicle,
+        marca,
+        ano: parseInt(ano),
+        descricao,
+        vendido: vendido === 'true', // converter para booleano
+      },
+    });
+    res.json(updatedVehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao atualizar veículo');
+  }
+});
+
+// Deleta um veiculo pelo seu ID
 app.delete('/veiculos/:id', async (req, res) => {
     const { id } = req.params;
     
@@ -59,52 +103,7 @@ app.delete('/veiculos/:id', async (req, res) => {
       res.status(500).json({ error: 'Error deleting vehicle' });
     }
   });
-  //Rota para pesquisar por parametro
-  app.get('/veiculos/:marca', async (req, res) => {
-    const { marca } = req.params;
-  
-    const vehicles = await prisma.vehicle.findMany({
-      where: {
-        marca: marca
-      }
-    });
-  
-    res.json(vehicles);
-  });
-  
-  //Rota para alterar algo especifico
-  app.patch('/veiculos/:id', async (req, res) => {
-    const { id } = req.params;
-    const { vehicle, marca, ano, descricao, vendido } = req.body;
-    
-    try {
-      const updatedVehicle = await prisma.vehicle.update({
-        where: { id: parseInt(id) },
-        data: {
-          vehicle,
-          marca,
-          ano: parseInt(ano),
-          descricao,
-          vendido: vendido === 'true', // converter para booleano
-        },
-      });
-      
-      res.json(updatedVehicle);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Erro ao atualizar veículo');
-    }
-  });
 
-  //Rota para visualizar um veiculo pelo id
-  app.get('/veiculos/:id', async (req, res) => {
-    const { id } = req.params;
-    const veiculo = await prisma.veiculo.findUnique({
-      where: { id: parseInt(id) }
-    });
-    res.json(veiculo);
-  });
-  
 app.listen(4002, () => {
   console.log('Servidor esta rodando');
 });
